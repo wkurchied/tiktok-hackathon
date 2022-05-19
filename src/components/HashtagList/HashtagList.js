b35f1fd4d99de70fad9d826aa497469ec5402b2a
Solène Jeanson <solene.jeanson@newswhip.com>
2022-05-19 11:49:44 +0100

    Styling

--- src/components/HashtagList/HashtagList.js
+++ src/components/HashtagList/HashtagList.js
@@ -4,7 +4,7 @@ import "./HashtagList.css";
 function HashtagList({hashtags}) {
     return ( 
         <div className="HashtagListContainer">
-            <h2>Trending</h2>
+            <h2 className="HashtagListTitle">Trending</h2>
             <div className="HashtagList">
                 {hashtags.map((hashtag, index) => (
                     <HashtagListItem key={index} hashtag={hashtag}/>
