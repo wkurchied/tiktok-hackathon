52471eea5af9185622dc216367668fabde184796
Walid Kurchied <walid.kurchied@newswhip.com>
2022-05-19 13:25:52 +0100

    css changes and small additona

--- src/components/TiktokListItemData/TiktokListItemLeftContent.jsx
+++ src/components/TiktokListItemData/TiktokListItemLeftContent.jsx
@@ -7,9 +7,9 @@ const TiktokListItemLeftContent = ({ videoContent }) => {
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkBxxlPDkOdmKZW7btolKxZCDZcFXnf_9Axw&usqp=CAU" className="content-thumbnail" />
       
       <div className="video-content-container">
-        <strong className="content-username">{videoContent.username}</strong>
-        <p className="content-description">{videoContent.description}</p>
-        <p className="content-hashtags">{videoContent.tags}</p>
+        Username: <strong className="content-username">{videoContent.username}</strong>
+        Description: <p className="content-description">{videoContent.description}</p>
+        Tags: <p className="content-hashtags">{videoContent.tags}</p>
       </div>
     </div>
   )
