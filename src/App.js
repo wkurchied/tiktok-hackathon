b96cc8aefeec4756bced9678c3a3669b736a953c
Solène Jeanson <solene.jeanson@newswhip.com>
2022-05-19 13:51:42 +0100

    Update setting of hashtags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FakeTokHeader from './components/FakeTokHeader/FakeTokHeader';
 import HashtagList from './components/HashtagList/HashtagList';
-import './App.css';
 import TiktokList from './components/TiktokList';
+import './App.css';
 
 const mockedTikTokData = [
   {username: 'walid', description: "some video content", tags: ["tag1", "tag2", "tag3"], viewsCount: 12, sharesCount: 111, likesCount: 2132},
@@ -11,16 +11,18 @@ const mockedTikTokData = [
   {username: 'walid', description: "some video content", tags: ["tag1", "tag2", "tag3"], viewsCount: 12, sharesCount: 111, likesCount: 2132},
 ]
 function App() {
-  const [hashtags] = useState(["test1hgiusheiruhewiurhfiwshfdiaushfidshufliushfiuh", "test2sojfosijfoijsofijsoijfoijsdofijsoidjf", "test3shfsdhfiushfoshfuhiufh", "test4", "test5", "test6", "test7"]);
+  const [hashtags, setHashtags] = useState([]);
   const [selectedHashtag, setSelectedHashtag] = useState("");
 
   const selectHashtag = (hashtag) => {
     setSelectedHashtag(hashtag)
   }
   // TODO: get hashtags from BE
-/*   useEffect(() => {
-       
-   }, [hashtags]); */
+  useEffect(() => {
+    if (hashtags.length === 0) {
+      setHashtags(["test2sojfosijfoijsofijsoijfoijsdofijsoidjf", "test3shfsdhfiushfoshfuhiufh", "test4", "test5", "test6", "test7"]);
+    }
+   }, [hashtags]);
 
   return (
     <div className="App">
