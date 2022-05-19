5672c425fab45c9d54844c40f6431f2b432fc975
Solène Jeanson <solene.jeanson@newswhip.com>
2022-05-19 16:18:28 +0100

    code clean up

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,5 +1,5 @@
 export const getHashtagList = () => {
-    return fetch("http://127.0.0.1:8000/hashtags").then((response)=>{
-        return response.json();
-      }).then(data => data)
+    return fetch("http://127.0.0.1:8000/hashtags")
+    .then( response => response.json())
+    .then(data => data);
 }
