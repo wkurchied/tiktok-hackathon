94a3ef7ffd60157a0f6d0264c1279c64d13d7c0d
Walid Kurchied <walid.kurchied@newswhip.com>
2022-05-19 14:18:52 +0100

    added style for icons and new date item

--- src/components/TiktokListItemData/TiktokListItemLeftContent.jsx
+++ src/components/TiktokListItemData/TiktokListItemLeftContent.jsx
@@ -7,9 +7,25 @@ const TiktokListItemLeftContent = ({ videoContent }) => {
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkBxxlPDkOdmKZW7btolKxZCDZcFXnf_9Axw&usqp=CAU" className="content-thumbnail" />
       
       <div className="video-content-container">
-        Username: <strong className="content-username">{videoContent.username}</strong>
-        Description: <p className="content-description">{videoContent.description}</p>
-        Tags: <p className="content-hashtags">{videoContent.tags}</p>
+        <div className="video-content-item">
+          <span className="video-content-heading">Username: </span> 
+          <span className="video-content-value">{videoContent.username}</span>
+        </div>
+
+        <div className="video-content-item">
+          <span className="video-content-heading">Description: </span> 
+          <span className="video-content-value">{videoContent.description}</span>
+        </div>
+
+        <div className="video-content-item">
+          <span className="video-content-heading">Created on: </span> 
+          <span className="video-content-value">11/11/2011</span>
+        </div>
+
+        <div className="video-content-item">
+          <span className="video-content-heading">Tags: </span> 
+          <span className="video-content-value">{videoContent.tags}</span>
+        </div>
       </div>
     </div>
   )
